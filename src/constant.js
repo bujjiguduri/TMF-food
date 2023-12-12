ee87f69c2ed7ff74ab9e6cf9445ada78c30cfec3
bujjiguduri <147382507+bujjiguduri@users.noreply.github.com>
2023-12-12 12:43:34 +0530

    Delete src directory

--- src/constant.js
+++
@@ -1,3 +0,0 @@
-export const APP_LOGO = 'https://image.similarpng.com/very-thumbnail/2021/09/Good-food-logo-design-on-transparent-background-PNG.png';
-
-export const RES_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
